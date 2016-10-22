a73ad6d82956b55ca472b886d141b2f5f07f0d83
febbraiod <donedesign1@gmail.com>
2016-10-22 17:20:58 -0400

    trying to get keys into api call

--- web/assets/js/file.js
+++ web/assets/js/file.js
@@ -4,7 +4,7 @@ $(function() {
 
 function apicall(){
   $('#button').click(function(){
-    $.get("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3",
+    $.get("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3", {client_id: getenv('FOURSQUARE_CLIENT_ID'), client_secret: getenv('FOURSQUARE_SECRET')},
       function(data){$(".result").html(data);});
   });
 }
