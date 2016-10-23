fde3d14383bfba0a5c266580163a511ff3276318
febbraiod <donedesign1@gmail.com>
2016-10-22 21:56:22 -0400

    added version date param to request

--- web/assets/js/file.js
+++ web/assets/js/file.js
@@ -8,7 +8,8 @@ function apicall(){
     // var secret = $().data();
     $.get("https://api.foursquare.com/v2/venues/categories", 
       {client_id: "MUUGJ2ZLK23TYMNXL1LAYRKP0LC4AX2JG0ZWCQTDJVLFS1VK", 
-      client_secret: "EMKL5KL2TIQJLK1GTSZFYXOXPD0CVZSSK4CILB3WZ1QYCPZO"},
+      client_secret: "EMKL5KL2TIQJLK1GTSZFYXOXPD0CVZSSK4CILB3WZ1QYCPZO",
+      v: '20161016'},
       function(data){$(".result").html(data);});
   });
 }
