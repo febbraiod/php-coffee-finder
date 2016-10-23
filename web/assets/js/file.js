f6dab271fefab4af63d5282693ef272bb5d20702
febbraiod <donedesign1@gmail.com>
2016-10-22 21:43:56 -0400

    trying api call with keys directly in jquery function

--- web/assets/js/file.js
+++ web/assets/js/file.js
@@ -4,9 +4,11 @@ $(function() {
 
 function apicall(){
   $('#button').click(function(){
-    var id = $().data();
-    var secret = $().data();
-    $.get("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3", {client_id: id, client_secret: secret},
+    // var id = $().data();
+    // var secret = $().data();
+    $.get("https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3", 
+      {client_id: "MUUGJ2ZLK23TYMNXL1LAYRKP0LC4AX2JG0ZWCQTDJVLFS1VK", 
+      client_secret: "EMKL5KL2TIQJLK1GTSZFYXOXPD0CVZSSK4CILB3WZ1QYCPZO"},
       function(data){$(".result").html(data);});
   });
 }
