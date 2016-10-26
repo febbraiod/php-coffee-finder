33b9ce3df61ae30eef0f3c2cf46f041dd660ccc5
febbraiod <donedesign1@gmail.com>
2016-10-26 11:33:54 -0400

    added a db(I hope) and fix a bit of css

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -90,6 +90,14 @@ class ComposerStaticInitd1f3219e9db453727220df4f7966c7d1
                 0 => __DIR__ . '/..' . '/pimple/pimple/lib',
             ),
         ),
+        'H' => 
+        array (
+            'Herrera\\Pdo' => 
+            array (
+                0 => __DIR__ . '/..' . '/herrera-io/pdo-log/src/lib',
+                1 => __DIR__ . '/..' . '/herrera-io/silex-pdo/src/lib',
+            ),
+        ),
     );
 
     public static function getInitializer(ClassLoader $loader)
--- web/index.php
+++ web/index.php
@@ -5,6 +5,16 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// Register Database
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -24,6 +34,22 @@ $app->get('/', function() use($app) {
   // return
   // array('id' => $id, 'secret' => $secret)
   return $app['twig']->render('index.twig', array("don" => $don));
+});
+
+// renders all rows in specified table
+$app->get('/db/', function() use($app) {
+  $it = $app['pdo']->prepare('SELECT * FROM image_table');
+  $it->execute();
+
+  $images = array();
+  while ($row = $it->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row);
+    $imageTableRows[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'imageTableRows' => $imageTableRows
+  ));
 });
 
 $app->run();
