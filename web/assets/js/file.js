cee2346dc81d55edb12e2bdeb86bf586a0500d26
febbraiod <donedesign1@gmail.com>
2016-10-27 11:52:00 -0400

    playing with the 4square api

--- web/assets/js/file.js
+++ web/assets/js/file.js
@@ -6,10 +6,14 @@ function apicall(){
   $('#button').click(function(){
     // var id = $().data();
     // var secret = $().data();
-    $.get("https://api.foursquare.com/v2/venues/categories", 
+    $.get("https://api.foursquare.com/v2/venues/search", 
       {client_id: "MUUGJ2ZLK23TYMNXL1LAYRKP0LC4AX2JG0ZWCQTDJVLFS1VK", 
       client_secret: "EMKL5KL2TIQJLK1GTSZFYXOXPD0CVZSSK4CILB3WZ1QYCPZO",
-      v: '20161016'},
+      v: '20161016',
+      near: 'Astoria, NY',
+      query: 'coffee',
+      limit: 5,
+      intent: 'browse'},
       function(data){
         debugger;
         $(".result").html(data);});
