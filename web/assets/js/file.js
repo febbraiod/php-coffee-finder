e73a4d377f83e934e80da392aff884fecb545d0f
febbraiod <donedesign1@gmail.com>
2016-10-28 15:59:51 -0400

    checking api response

--- web/assets/js/file.js
+++ web/assets/js/file.js
@@ -5,15 +5,13 @@ $(function() {
 
 function apicall(){
   $('#button').click(function(){
-    // var id = $().data();
-    // var secret = $().data();
     var city = $('#get_city').val();
-    debugger
+
     $.get("https://api.foursquare.com/v2/venues/search", 
       {client_id: "MUUGJ2ZLK23TYMNXL1LAYRKP0LC4AX2JG0ZWCQTDJVLFS1VK", 
       client_secret: "EMKL5KL2TIQJLK1GTSZFYXOXPD0CVZSSK4CILB3WZ1QYCPZO",
       v: '20161016',
-      near: 'Norwalk, CT',
+      near: city,
       radius: 5000,
       query: 'coffee',
       limit: 5,
